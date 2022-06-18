0e0a6cbfc4909363ef25f9ffec6594c1ea928c33
Marymuriga <marymuriga@gmail.com>
2022-06-19 01:02:47 +0300

    commit tree 2a068535927887b3273007e3fe74c209d9498b9a

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Write your algorithm 
+  const nums = {}
+  for (let i = 0; i<array.length; i++){
+  const num = array[i]
+if( target- num in nums){
+  return [nums[target-num], i]
 }
-
+nums [array[i]] = i
+}
+return [-1, -1]
+}
 /* 
   Write the Big O time complexity of your function here
 */
-
+// O(n) linear we are iterating through every elements in an array
+// O(1) constant accessing a value by index
 /* 
   Add your pseudocode here
 */
+// empty variable to hold the element
+// iteration through every element in the array 
+// comparing the element to see which pair equals to the target
+// if the pair matches return true or else false
+// 
 
 /*
   Add written explanation of your solution here
@@ -29,6 +43,28 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([-7, 10, 4, 8], 3));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([1, 2, 3, 4], 5));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([2, 2, 3, 3], 4));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([4], 5));
+
+  console.log("");
 }
 
 module.exports = hasTargetSum;
